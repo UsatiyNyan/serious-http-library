891d1c44a7766923fb78573d4b203deb542ebf5f
UsatiyNyan <us4tiyny4n@gmail.com>
2026-05-20 21:29:16 +0200

    types: refactoring

--- include/sl/http/v1/types/fields.hpp
+++ include/sl/http/v1/types/fields.hpp
@@ -12,21 +12,20 @@
 #include <string_view>
 
 namespace sl::http::v1 {
+namespace detail {
 
 struct string_hash {
     using is_transparent = void;
-    std::size_t operator()(std::string_view sv) const noexcept {
-        return std::hash<std::string_view>{}(sv);
-    }
+    std::size_t operator()(std::string_view sv) const noexcept { return std::hash<std::string_view>{}(sv); }
 };
 
 struct string_equal {
     using is_transparent = void;
-    bool operator()(std::string_view lhs, std::string_view rhs) const noexcept {
-        return lhs == rhs;
-    }
+    bool operator()(std::string_view lhs, std::string_view rhs) const noexcept { return lhs == rhs; }
 };
 
-using fields_type = tsl::robin_map<std::string, std::string, string_hash, string_equal>;
+} // namespace detail
+
+using fields_type = tsl::robin_map<std::string, std::string, detail::string_hash, detail::string_equal>;
 
 } // namespace sl::http::v1
